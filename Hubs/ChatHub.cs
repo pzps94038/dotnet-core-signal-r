329c379348b331ca624054e86c3eeb8220c2bf42
ron.ro <pzps94038@yahoo.com.tw>
2023-06-11 19:23:42 +0800

    feat: 初始化專案

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using dotnet_core_signal_r.Core;
+using dotnet_core_signal_r.Model.Shared;
+using Microsoft.AspNetCore.SignalR;
+
+namespace dotnet_core_signal_r.Hubs
+{
+    public class ChatHub : Hub
+    {
+        /// <summary>
+        /// 在線聊天室
+        /// </summary>
+        private static Dictionary<string, string> liveChatRoom = new Dictionary<string, string>();
+
+        /// <summary>
+        /// 加入聊天室
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <returns></returns>
+        public async Task<BaseResponse> JoinLiveChatRoom(string userName)
+        {
+            var result = new BaseResponse<string>();
+            try
+            {
+                string? connectionId;
+                if (liveChatRoom.TryGetValue(userName, out connectionId))
+                {
+                    result.ReturnCode = ReturnCode.Unique.Description();
+                    result.ReturnMessage = ReturnMessage.Unique.Description();
+                }
+                else
+                {
+                    liveChatRoom.Add(userName, Context.ConnectionId);
+                    // 更新在線清單給全部人
+                    await Clients.All.SendAsync("PublicLiveChatRoom", liveChatRoom);
+                    result.ReturnCode = ReturnCode.Success.Description();
+                    result.ReturnMessage = ReturnMessage.CreateSuccess.Description();
+                    result.Data = Context.ConnectionId;
+                }
+            }
+            catch (Exception)
+            {
+                result.ReturnCode = ReturnCode.Fail.Description();
+                result.ReturnMessage = ReturnMessage.Fail.Description();
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// 公開聊天室留言
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public async Task<BaseResponse> PublicChatSendMessage(string message)
+        {
+            var result = new BaseResponse();
+            try
+            {
+                var userName = liveChatRoom.Where(a => a.Value == Context.ConnectionId).Select(a => a.Key).SingleOrDefault();
+                if (userName != null)
+                {
+                    result.ReturnCode = ReturnCode.Success.Description();
+                    result.ReturnMessage = ReturnMessage.Success.Description();
+                    // 發送公開訊息給全部人
+                    await Clients.All.SendAsync("PublicMessage", userName, message);
+                }
+                else
+                {
+                    result.ReturnCode = ReturnCode.NotFound.Description();
+                    result.ReturnMessage = ReturnMessage.NotFound.Description();
+                }
+            }
+            catch (Exception)
+            {
+                result.ReturnCode = ReturnCode.Fail.Description();
+                result.ReturnMessage = ReturnMessage.Fail.Description();
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// 私密聊天室留言
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public async Task<BaseResponse> PrivateChatSendMessage(string connectionId, string message)
+        {
+            var result = new BaseResponse();
+            try
+            {
+                var privateUser = liveChatRoom.Where(a => a.Value == connectionId).Select(a => new
+                {
+                    Id = a.Value,
+                    Name = a.Key
+                }).SingleOrDefault();
+                if (privateUser != null)
+                {
+                    var sendUser = liveChatRoom.Where(a => a.Value == Context.ConnectionId).Select(a => new
+                    {
+                        Id = a.Value,
+                        Name = a.Key
+                    }).Single();
+                    result.ReturnCode = ReturnCode.Success.Description();
+                    result.ReturnMessage = ReturnMessage.Success.Description();
+                    // 發送私訊給指定人
+                    await Clients.Client(privateUser.Id).SendAsync("PrivateMessage", sendUser.Name, message);
+                }
+                else
+                {
+                    result.ReturnCode = ReturnCode.NotFound.Description();
+                    result.ReturnMessage = ReturnMessage.NotFound.Description();
+                }
+            }
+            catch (Exception)
+            {
+                result.ReturnCode = ReturnCode.Fail.Description();
+                result.ReturnMessage = ReturnMessage.Fail.Description();
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// 離線處理
+        /// </summary>
+        /// <param name="exception"></param>
+        /// <returns></returns>
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            var userName = liveChatRoom.Where(a => a.Value == Context.ConnectionId).Select(a => a.Key).SingleOrDefault();
+            if (userName != null)
+            {
+                liveChatRoom.Remove(userName);
+                // 更新在線清單給全部人
+                await Clients.All.SendAsync("PublicLiveChatRoom", liveChatRoom);
+            }
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
+
---
+++ Model/Shared/BaseResponse.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace dotnet_core_signal_r.Model.Shared
+{
+    public class BaseResponse<T> : BaseResponse
+    {
+        public T Data { get; set; }
+    }
+
+    public class BaseResponse
+    {
+        /// <summary>
+        /// 回傳代碼
+        /// </summary>
+        public string ReturnCode { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 回傳訊息
+        /// </summary>
+        public string ReturnMessage { get; set; } = string.Empty;
+    }
+
+    public enum ReturnCode
+    {
+        [Description("00")]
+        Success,
+        [Description("96")]
+        AuthExpired,
+        [Description("97")]
+        Unique,
+        [Description("98")]
+        NotFound,
+        [Description("99")]
+        Fail
+    }
+
+    public enum ReturnMessage
+    {
+        [Description("取得資料成功")]
+        Success,
+        [Description("取得資料失敗")]
+        Fail,
+        [Description("新增資料成功")]
+        CreateSuccess,
+        [Description("新增資料失敗")]
+        CreateFail,
+        [Description("修改資料成功")]
+        ModifySuccess,
+        [Description("修改資料失敗")]
+        ModifyFail,
+        [Description("刪除資料成功")]
+        DeleteSuccess,
+        [Description("刪除資料失敗")]
+        DeleteFail,
+        [Description("登入成功")]
+        LoginSuccess,
+        [Description("帳號或密碼錯誤")]
+        LoginFail,
+        [Description("找不到資料")]
+        NotFound,
+        [Description("已有重複資料")]
+        Unique,
+        [Description("系統發生錯誤")]
+        SystemFail,
+        [Description("身分驗證過期")]
+        AuthExpired,
+        [Description("身分驗證失敗")]
+        AuthFail,
+        [Description("寄送信件成功")]
+        SendMailSuccess,
+        [Description("寄送信件失敗")]
+        SendMailFail,
+        [Description("上傳成功")]
+        UploadSuccess,
+        [Description("上傳失敗")]
+        UploadFail
+    }
+}
+
